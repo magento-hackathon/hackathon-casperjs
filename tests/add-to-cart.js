a2479f025c6423db6ad7fb516d3c1d2847c48132
Don Bosco van Hoi <bvanhoi@mothership.de>
2013-10-26 16:43:27 +0200

    Added test for adding product to cart

---
+++ tests/add-to-cart.js
@@ -0,0 +1,51 @@
+casper.test.begin('Add To Cart', function suite(test) {
+
+    // Start page
+    casper.start(url + 'apparel.html', function () {
+
+        this.printTitle();
+
+        test.info('Testing Login');
+        test.info('Target URL: ' + url);
+
+        test.assertUrlMatch(url + 'apparel.html');
+
+        // check, that the first product has a buy button
+
+        // check that the home page has the link login
+        test.assertExists('button.button.btn-cart:first-child');
+        this.click('button.button.btn-cart:first-child');
+
+    })
+
+    .then(function() {
+
+        this.printTitle();
+
+        test.assertUrlMatch(url + 'coalesce-functioning-on-impatience-t-shirt.html');
+
+        this.evaluate(function() {
+            var elmt = document.querySelector('select#attribute525');
+            elmt.selectedIndex = 2;
+            elmt.onchange();
+        });
+
+        // check that the home page has the link login
+        test.assertExists('button.button.btn-cart');
+        this.click('button.button.btn-cart');
+
+    })
+
+    .then(function() {
+
+        this.printTitle();
+
+        test.assertUrlMatch(url + 'checkout/cart');
+
+    })
+
+    .run(function () {
+        test.done();
+    });
+});
+
