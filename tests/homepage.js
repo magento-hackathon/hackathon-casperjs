9206724b8128418702087177179117d767295ac0
Don Bosco van Hoi <bvanhoi@mothership.de>
2013-10-26 16:25:48 +0200

    Enhanced test for homepage. Finalized test for login.js

--- tests/homepage.js
+++ tests/homepage.js
@@ -3,18 +3,18 @@ casper.test.begin('Homepage', function suite(test) {
     // Start page
     casper.start(url, function () {
 
+        this.printTitle();
+
         test.info('Testing Homepage');
 
         /**
          * User this command to get the url from the command line.
          */
-        var targetUrl = casper.cli.get('url')
-
-        test.info('Target URL: ' + targetUrl);
+        test.info('Target URL: ' + url);
 
         test.assertTitle('Home page');
-        test.assertExists('.page .header-container .header h1.logo a[href="'+targetUrl+'/"]');
-        test.assertExists('.box table td:first-child a[href="'+targetUrl+'/sony-vaio-vgn-txn27n-b-11-1-notebook-pc.html"]')
+        test.assertExists('.page .header-container .header h1.logo a[href="' + url + '"]');
+        test.assertExists('.box table td:first-child a[href="' + url + 'sony-vaio-vgn-txn27n-b-11-1-notebook-pc.html"]')
     })
 
     .run(function () {
