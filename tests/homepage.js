fd7c5c1a6c7f977aab429955dbf734d51347b775
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2014-01-31 15:26:50 +0100

    [TEST] Perform homepage.js

--- tests/homepage.js
+++ tests/homepage.js
@@ -3,19 +3,14 @@ casper.test.begin('Homepage', function suite(test) {
     // Start page
     casper.start(url, function () {
 
-        this.printTitle();
-
-        test.info('Testing Homepage');
-
-        /**
-         * User this command to get the url from the command line.
-         */
-        test.info('Target URL: ' + url);
-
+        test.assertHttpStatus(200);
         test.assertTitle('Home page');
+        test.assertExists('body.cms-index-index');
         test.assertExists('.page .header-container .header h1.logo a[href="' + url + '"]');
-        test.assertExists('.page .col2-right-layout .main .col-main .std .page-title');
-        test.assertSelectorHasText('.page .col2-right-layout .main .col-main .std .page-title', 'Home Page');
+        test.assertExists('.col-left .block-tags');
+        test.assertExists('.col-right .block-compare');
+        test.assertExists('.col-right .block-poll');
+        test.assertElementCount('.best-selling td', 6);
     })
 
     .run(function () {
