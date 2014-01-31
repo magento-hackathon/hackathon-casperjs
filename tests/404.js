c7bcd6c6a82b0ee723db3ce03a38eea287ebd07e
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2014-01-31 15:42:56 +0100

    [TEST] Added 404.js

---
+++ tests/404.js
@@ -0,0 +1,17 @@
+casper.test.begin('404 Not Found', function suite(test) {
+
+    // Start page
+    casper.start(url + 'not-found', function () {
+
+        test.assertHttpStatus(404);
+
+        test.assertTitle('404 Not Found 1');
+        test.assertExists('body.cms-index-noroute');
+        test.assertSelectorHasText('.page-head-alt', 'Whoops, our bad...');
+    })
+
+    .run(function () {
+        test.done();
+    });
+});
+
